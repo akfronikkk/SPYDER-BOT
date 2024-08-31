f8cf6f272b0c4d7e8d305e9a0493e27da62fe635
akfronikkk <earneasyakfronik@gmail.com>
2024-08-31 16:22:30 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -7,7 +7,6 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQ
 from pyrogram import Client, filters, enums 
 from pyrogram.errors import FloodWait, UserIsBlocked, MessageNotModified, PeerIdInvalid
 from utils import get_size, is_subscribed, get_poster, search_gagala, temp, get_settings, save_group_settings
-from pinterest_utils import download_pinterest_photo, fetch_image
 from instagram_utils import get_instagram_media, fetch_instagram_media
 from database.users_chats_db import db
 from database.ia_filterdb import Media, get_file_details, get_search_results
